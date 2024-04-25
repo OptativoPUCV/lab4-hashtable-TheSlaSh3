e6c9cd22b57cfbf14fb87bda33c820b4de9bd408
TheSlaSh3 <132639459+TheSlaSh3@users.noreply.github.com>
2024-04-25 04:09:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -38,15 +38,6 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
-/*Implemente la función void insertMap(HashMap * map, char * key, void * value). Esta función inserta un nuevo dato (key,value) en el mapa y actualiza el índice current a esa posición. Recuerde que para insertar un par (clave,valor) debe:
-
-  a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
-
-  b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (método de resolución de colisiones). Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
-
-  c - Ingrese el par en la casilla que encontró.
-
-  No inserte claves repetidas. Recuerde que el arreglo es circular. Recuerde actualizar la variable size.*/
 void insertMap(HashMap * map, char * key, void * value) {
   long posicion = hash(key, map->capacity);
   long posicionOriginal = posicion;
@@ -71,7 +62,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-
+  
 
 }
 
@@ -98,8 +89,16 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  long i; 
+  for(i = 0; i <map->capacity; i++)
+  {
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }    
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
